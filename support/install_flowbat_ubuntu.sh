d957a483d425a9caa292a1aac91d6b9181783db6
Jason Smith <jason.smith@mandiant.com>
2014-10-13 15:18:32 -0400

    changed a lot of stuff, fixed base directory installation, fixed upstart, fixed aesthetics, etc...

--- support/install_flowbat_ubuntu.sh
+++ support/install_flowbat_ubuntu.sh
@@ -1,32 +1,65 @@
 #!/bin/bash
 
-if [ "$1" = "--update" ]; then
-echo "$(tput setaf 4)This script will install flowbat updates automatically. If it is not up-to-date, it will require a FlowBAT restart.$(tput sgr0)"
-        read -p "$(tput setaf 3)Are you sure you want to update?$(tput sgr0)" -n 1 -r
-        echo
-        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-                exit 1
+workingDir=$PWD
+
+ask() {
+    # http://djm.me/ask
+    while true; do
+
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
         else
-		cd /home/"$USER"/opt/FlowBAT/
+            prompt="y/n"
+            default=
+        fi
+
+        # Ask the question
+        read -p "$1 [$prompt] " REPLY
+
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y|y) return 0 ;;
+            N|n) return 1 ;;
+        *) echo "You must give a y or n answer." ;;
+        esac
+
+    done
+}
+
+if [ "$1" = "--update" ]; then
+echo "$(tput setaf 6)This script will install flowbat updates automatically. If it is not up-to-date, it will require a FlowBAT restart.$(tput sgr0)"
+        if ask "$(tput setaf 3)Are you sure you want to update?$(tput sgr0)"; then
+		cd "$workingDir"/FlowBAT/
 		git pull
 		exit
+  else
+    exit 1
 	fi
 fi
 
-if [ ! -d "/home/$USER/opt/FlowBAT/" ]; then
-  echo "$(tput setaf 4)This script will install flowbat locally, to be accessed at http://localhost:1800$(tput sgr0)"
-	read -p "$(tput setaf 3)Are you sure you want to install?$(tput sgr0)" -n 1 -r
-	echo    # (optional) move to a new line
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+if [ ! -d "$workingDir/FlowBAT/" ]; then
+  echo "$(tput setaf 6)This script will install flowbat locally, to be accessed at http://localhost:1800$(tput sgr0)"
+	if ask "$(tput setaf 3)Are you sure you want to install?$(tput sgr0)"; then
+    echo
+  else
 		exit 1
 	fi
 fi
 
-echo "$(tput setaf 4)Checking installed packages...$(tput sgr0)"
+echo "$(tput setaf 6)Checking installed packages...$(tput sgr0)"
 sudo apt-get update -qq
 function testinstall {
 	dpkg -s "$1" &> /dev/null || {
-	    printf '%s\n' "$(tput setaf 4)$1 not installed. Attempting install.$(tput sgr0)" >&2
+	    printf '%s\n' "$(tput setaf 6)$1 not installed. Attempting install.$(tput sgr0)" >&2
 	    sudo apt-get install -qq -y "$1"
 	}
 }
@@ -36,48 +69,42 @@ testinstall checkinstall
 testinstall curl
 testinstall git-core
 
-if [ ! -d "/home/$USER/opt/FlowBAT/" ]; then
-  mkdir opt
-fi
-
-cd opt/
-
-if [ ! -d "/home/$USER/opt/FlowBAT/" ]; then
-	echo "$(tput setaf 4)Downloading FlowBAT...$(tput sgr0)"
+if [ ! -d "$workingDir/FlowBAT/" ]; then
+	echo "$(tput setaf 6)Downloading FlowBAT...$(tput sgr0)"
 	git clone https://github.com/chrissanders/FlowBAT.git
 fi
 cd FlowBAT/
 
 echo""
-echo "$(tput setaf 4)Checking for nodejs...$(tput sgr0)"
+echo "$(tput setaf 6)Checking for nodejs...$(tput sgr0)"
 dpkg -s nodejs &> /dev/null || {
 	printf '%s\n' "nodejs not installed. Attempting install." >&2
 	curl -sL https://deb.nodesource.com/setup | sudo bash -
-  printf '%s\n' "$(tput setaf 4)nodejs installing. This may take a minute.$(tput sgr0)" >&2
+  printf '%s\n' "$(tput setaf 6)nodejs installing. This may take a minute.$(tput sgr0)" >&2
 	sudo apt-get install -qq -y nodejs
         }
 
 echo""
-echo "$(tput setaf 4)Checking for meteorite...$(tput sgr0)"
+echo "$(tput setaf 6)Checking for meteorite...$(tput sgr0)"
 if ! which mrt > /dev/null; then
-	echo -e "$(tput setaf 4)Meteorite not found! Installing...$(tput sgr0)"
+	echo -e "$(tput setaf 6)Meteorite not found! Installing...$(tput sgr0)"
 	sudo npm install --silent -g meteorite
 fi
 echo ""
-echo "$(tput setaf 4)Checking for meteor...$(tput sgr0)"
+echo "$(tput setaf 6)Checking for meteor...$(tput sgr0)"
 if ! which meteor > /dev/null; then
-        echo -e "$(tput setaf 4)Meteor not installed. Attempting Install.$(tput sgr0)"
+        echo -e "$(tput setaf 6)Meteor not installed. Attempting Install.$(tput sgr0)"
         curl https://install.meteor.com | /bin/sh
 fi
 
 #Arranging for localhost configuration
 cat settings/prod.sample.json |  sed 's/flowbat.com/127.0.0.1:1800/' | sed 's/mailUrl.*/mailUrl": "",/' > settings/dev.json
 
-cd /home/"$USER"/opt/FlowBAT
+cd "$workingDir"/FlowBAT
 mrt install
 
 #Generating upstart configuration for FlowBAT
-cat > flowbat.conf << "EOF"
+cat <<EOF > flowbat.conf
 # meteorjs - FlowBAT job file
 
 description "FlowBAT"
@@ -94,21 +121,21 @@ respawn
 # Essentially lets upstart know the process will detach itself to the background
 expect fork
 
-chdir /home/$USER/opt/FlowBAT
+chdir $workingDir/FlowBAT
 
 script
 
-cd /home/$USER/opt/FlowBAT
-exec sudo -u $USER meteor --port 1800 run --settings /home/$USER/opt/FlowBAT/settings/dev.json "$@"
+cd $workingDir/FlowBAT
+exec sudo -u $USER meteor --port 1800 run --settings $workingDir/FlowBAT/settings/dev.json testtest
 
 end script
 EOF
 
+sed -i 's/testtest/\"$@"/g' flowbat.conf
+
 if [ ! -f /etc/init/flowbat.conf ]; then
-	read -p "$(tput setaf 3)Do you wish to have FlowBAT start on boot in the background?$(tput sgr0)" -n 1 -r
-        	echo
-	        if [[ $REPLY =~ ^[Yy]$ ]]; then
-	                sudo cp flowbat.conf /etc/init/
+	if ask "$(tput setaf 3)Do you wish to have FlowBAT start on boot in the background?$(tput sgr0)"; then
+      sudo cp flowbat.conf /etc/init/
 		else
 			echo "$(tput setaf 2)For future reference, move flowbat.conf to /etc/init/ if you would like to have FlowBAT start on boot.$(tput sgr0)".
 		fi
@@ -116,10 +143,10 @@ fi
 
 sudo chown -R "$USER":"$USER" /home/"$USER"/
 
-echo -e "$(tput setaf 2)To manually run FlowBAT, cd to /home/$USER/opt/FlowBAT and run:"
+echo -e "$(tput setaf 2)To manually run FlowBAT, cd to $workingDir/FlowBAT and run:"
 echo -e 'meteor --port 1800 run --settings settings/dev.json "$@"'
-echo -e 'or in the background:'
-echo -e 'nohup meteor --port 1800 run --settings /home/$USER/opt/FlowBAT/settings/dev.json "$@" &'
+echo -e 'or to run FlowBAT in the background:'
+echo -e 'nohup meteor --port 1800 run --settings settings/dev.json "$@" &'
 echo -e "$(tput sgr0)"
 
 echo "$(tput setaf 2)Attempting startup. This may take a few minutes if it is the first time. Press ctrl+c to stop FlowBAT after the application says it is running or proceed to 127.0.0.1:1800 in a browser.$(tput sgr0)"
